1fbf9dc64235bd8c494ccd1a3ee8844d4ec0e430
okatarismet <okatarismet@gmail.com>
2022-02-04 17:07:56 +0300

    aa

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -6,24 +6,7 @@ import Post from '../Graphi/post.js'
 
 export default {
  
-   create: async (payload, callback) => {
-       const { userId, content } = payload.data; 
-       if(userId == null || content == null ){
-           return callback(error(ErrorMessages.post_field_missing))
-        }
 
-        let post = await Post.create({ 
-            content,
-        });
-        await User.addRelation(userId,'Posted',Post,post._id);
-        callback({
-            success: true,
-            data:{
-                users: await User.findAll()
-            }
-        })
-    },
-    
    update: async (payload, callback) => {
        const { postId, content } = payload.data; 
        if(postId == null || content == null ){
